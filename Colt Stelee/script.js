1585e80f5b9ac92083aa4bbb273c45699b1b4e4a
raviearjun <67139732+raviearjun@users.noreply.github.com>
2025-01-15 14:46:11 +0700

    Add files via upload

---
+++ Colt Stelee/script.js
@@ -0,0 +1,24 @@
+const strNumber = '123'
+const number = Number(strNumber)
+console.log(number)
+
+const strBool =  true;
+const bool = Boolean(strBool)
+console.log(bool)
+
+const strFloat = '3.14'
+const float = Number(strFloat)
+console.log(float)
+
+const cm  = '100cm'
+const px = '64px'
+
+const intFromCM = parseInt(cm)
+const intFromPX = parseInt(px)
+
+console.log(intFromCM)
+console.log(intFromPX)
+
+let age = 25;
+tes = (age<18) ? 'Back to your mama' : 'Welcome to the party';
+console.log(tes)
