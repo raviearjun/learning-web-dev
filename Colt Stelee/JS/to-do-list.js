a7514824f6659e706ee3ed94b3c17fb3508151ad
raviearjun <arjunnadhief@gmail.com>
2025-01-21 14:25:35 +0700

    belajar git

--- Colt Stelee/JS/to-do-list.js
+++ Colt Stelee/JS/to-do-list.js
@@ -1,32 +1,42 @@
-const todoName = prompt("What would you like to name your to-do list?");
-console.log(`NAMA TODO LIST : ${todoName} \n*************`);
-const todos = [];
-
-while(true){
-    let input = prompt("Enter the keyword");
-    if(input === 'new'){
-        newTodo = prompt('Enter new to-do list');
+function startTodoApp() {
+    const todoName = prompt("What would you like to name your to-do list?");
+    console.log(`NAMA TODO LIST : ${todoName} \n*************`);
+    const todos = [];
+  
+    while (true) {
+      let input = prompt("Enter the keyword");
+      if (input === 'new') {
+        const newTodo = prompt('Enter new to-do list');
         todos.push(newTodo);
         console.log(`${newTodo} added to the list`);
         console.log('**********');
-    }else if(input === 'list'){
-        for(let todo of todos){
-            console.log(todo);
+      } else if (input === 'list') {
+        console.log('Your Todo List:');
+        for (let todo of todos) {
+          console.log(todo);
         }
         console.log('**********');
-    }else if(input === 'delete'){
-        indexToDelete = prompt('Which activity do you want to delete? (enter the index, enter -1 to back)\n ' + todos);
-        if(indexToDelete === '-1') {
-            continue;
-        }else{
-            todos.splice(indexToDelete, 1);
-            console.log(`Activity ${todos[indexToDelete]} has been deleted`);
-            console.log('**********');
+      } else if (input === 'delete') {
+        const indexToDelete = prompt(
+          'Which activity do you want to delete? (enter the index, enter -1 to back)\n' +
+          todos.map((todo, i) => `${i} : ${todo}`).join('\n')
+        );
+        if (indexToDelete === '-1') {
+          continue;
+        } else if (indexToDelete >= 0 && indexToDelete < todos.length) {
+          const deletedTodo = todos.splice(indexToDelete, 1);
+          console.log(`Activity "${deletedTodo}" has been deleted`);
+          console.log('**********');
+        } else {
+          console.log('Invalid index!');
         }
-        
-    }else if(input === 'quit'){
+      } else if (input === 'quit') {
         console.log('OK, you quit!');
         console.log('**********');
         break;
+      } else {
+        console.log('Invalid input. Please try again.');
+      }
     }
-}
+  }
+  
