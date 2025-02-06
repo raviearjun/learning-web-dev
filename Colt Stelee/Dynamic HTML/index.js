0920b7f8bff37ced65cd9faea60830e59187686c
raviearjun <arjunnadhief@gmail.com>
2025-02-06 17:17:51 +0700

    belajar ejs

--- Colt Stelee/Dynamic HTML/index.js
+++ Colt Stelee/Dynamic HTML/index.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const serverData = require('./data.json');
 
-app.set('view engine', 'ejs') // express js di balik layar mencari view di folder views dan menjadikannya view engine
+// middleware
+app.use(express.static(path.join(__dirname, 'public_assets'))); 
+// kode di atas :
+// 1. menggunakan direktori public_assets dan menjadikannya public (bisa diakses user melalui url)
+// 2. express.static() memberikan 'apa adanya' file yang ada di dalam direktori public_assets
  
+app.set('view engine', 'ejs') // express js di balik layar mencari view di folder views dan menjadikannya view engine
+app.set('views', path.join(__dirname, '/views')) // path.join menggabungkan path ke direktori views
+// ini adalah praktik yang bagus mengingat kita kadang perlu mengeksekusi kode dari luar direktori ini
+
 app.get('/', (req, res) => {
     // res.send('Haloo sayangg!')
     // kita juga bisa mengirimkan file html, bukan hanya string seperti di atas
     // untuk itu kita perlu method res.render()
     res.render('home') // ini akan me-render view home.ejs di dalam folder/direktori views (by default)
     // atau kalau mau spesifik bisa dengan res.render('views/home.ejs')
+})
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1
+    res.render('random', {rand : num}) 
+    // merender file random.ejs. 
+    // variable num dikenal di file random.ejs dengan nama rand
+})
+
+app.get('/r/:subpath', (req, res) => {
+    const { subpath } = req.params;
+    const data = serverData[subpath];
+    console.log(data)
+    res.render('subpath.ejs', { ...data })
+})
+
+app.get('/cats', (req, res) => {
+    const cats = ['Luna', 'Milo', 'Oscar', 'Oreo']
+    res.render('cats', {cats})
 })
 
 app.listen(8080, () => {
--- Colt Stelee/Dynamic HTML/notes.md
+++ Colt Stelee/Dynamic HTML/notes.md
@@ -11,5 +11,9 @@ DYNAMIC HTML
    - EJS akan merender template dengan mengisi form yang harus diisi dengan data olahan dari Express
    - Hasil render diberikan balik ke Express untuk dikirimkan ke user
 4. Method `res.render()`
-   dalam Express JS akan mencari file template di folder bernama `views` (default)lalu menerima template yang sudah diproses oleh template engine untuk kemudian dikirimkan ulang ke user
-5. jajaj
+   Method dalam Express JS ini akan mencari file template di folder bernama `views` (default) lalu menerima template yang sudah diproses oleh template engine untuk kemudian dikirimkan ulang ke user. Method ini akan merender view dari html dinamis yang disediakan oleh EJS. Hasil pemorsesan oleh EJS disebut `view`
+5. Bagaimana jika kita ingin mengeksekusi views.ejs dari luar direktori yang sama?
+   Kita perlu memasang method 
+   `app.set('views', path.join(__dirname, '/views'))`
+   yang bermakna kita mengatur path untuk mengeksekusi file ini menjadi direktori index.js -> views
+6. 
