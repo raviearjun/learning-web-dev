bd6fb95af90069032c1f149bd806e867f9b164e3
raviearjun <arjunnadhief@gmail.com>
2025-05-01 19:18:00 +0700

    project omahti

---
+++ OTI/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views'); // set the views directory to the current dir where index js is located
+
+app.get('/', (req, res) => {
+  res.render('landing.ejs'); 
+});
+
+app.listen(3000, () => {    
+  console.log('Server is running on http://localhost:3000');
+});
