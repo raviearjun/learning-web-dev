e9e03764d053c9f645eb4ff24420344051a62d28
raviearjun <arjunnadhief@gmail.com>
2025-05-02 01:55:46 +0700

    coba menambahkan step-step pemesanan web

--- OTI/index.js
+++ OTI/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views'); // set the views directory to the current dir where index js is located
+// Serve static files from /assets
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
   res.render('landing.ejs'); 
